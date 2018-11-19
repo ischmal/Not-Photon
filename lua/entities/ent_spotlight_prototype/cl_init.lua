3e57dcd69707f5c062978200387877ecacedcda5
Alex Smith <i@schmal.me>
2018-11-19 14:30:27 -0700

    Made manipulation rate relative, made adjustments observe pose param range

--- lua/entities/ent_spotlight_prototype/cl_init.lua
+++ lua/entities/ent_spotlight_prototype/cl_init.lua
@@ -9,8 +9,11 @@ include("cl_debug_panel.lua")
 function ENT:Initialize()
 	self.LightBoneId = self:LookupBone(self.LightBone)
 	SPOTLIGHT_TARGET = self
-	self.YawParam = 0
-	self.PitchParam = 0
+	self.HorizontalParam = 0
+	self.VerticalParam = 0
+	timer.Simple(0, function()
+		self:SetupPoseParamsMap()
+	end)
 end
 
 function ENT:SetupLamp()
@@ -18,10 +21,8 @@ function ENT:SetupLamp()
 	lamp:SetTexture("effects/flashlight001")
 	lamp:SetNearZ(2)
 	lamp:SetFarZ(500)
-	-- lamp.
 	lamp:Update()
 	self.ProjectedTexture = lamp
-	-- self.ProjectedTexture
 end
 
 function ENT:RemoveLamp()
@@ -33,7 +34,6 @@ end
 function ENT:UpdateState()
 	local state = self:GetLightEnabled()
 	if state then
-		print("light on")
 		self:SetupLamp()
 		if istable(self.LampSubMaterials) then
 			for matId, mat in pairs(self.LampSubMaterials) do
@@ -41,7 +41,6 @@ function ENT:UpdateState()
 			end
 		end
 	else
-		print("light off")
 		if istable(self.LampSubMaterials) then
 			for matId, mat in pairs(self.LampSubMaterials) do
 				self:SetSubMaterial(matId, nil)
@@ -69,15 +68,59 @@ function ENT:Think()
 	if self:GetLightEnabled() then
 		self:UpdateLamp()
 	end
-	-- local deg = (math.sin(CurTime()) * 90)
-	-- self:SetPoseParameter("point_yaw", deg)
-	self:SetPoseParameter(self.LampPoseParams[2], self.YawParam)
-	-- self:SetPoseParameter("point_pitch", deg)
-	self:SetPoseParameter(self.LampPoseParams[1], self.PitchParam)
+	self:SetPoseParameter(self.LampPoseParams[2], self.HorizontalParam)
+	self:SetPoseParameter(self.LampPoseParams[1], self.VerticalParam)
+	self:InvalidateBoneCache()
 end
 
 function ENT:OnRemove()
 	self:RemoveLamp()
+end
+
+local MOVE_UP = 0
+local MOVE_DOWN = 1
+local MOVE_LEFT = 2
+local MOVE_RIGHT = 3
+
+function ENT:Adjust(dir)
+	local min, max
+	if (dir == MOVE_UP or dir == MOVE_DOWN) then
+		local manip = self.PoseManipulationRates[self.LampPoseParams[1]]
+		min, max = self:GetPoseParameterRange(self.PoseParamsMap[self.LampPoseParams[1]])
+		if dir == MOVE_UP then
+			local newVal = self.VerticalParam - manip
+			if newVal >= min then
+				self.VerticalParam = newVal
+			else
+				self.VerticalParam = min
+			end
+		else
+			local newVal = self.VerticalParam + manip
+			if newVal <= max then
+				self.VerticalParam = newVal
+			else
+				self.VerticalParam = max
+			end
+		end
+	elseif (dir == MOVE_LEFT or dir == MOVE_RIGHT) then
+		local manip = self.PoseManipulationRates[self.LampPoseParams[2]]
+		min, max = self:GetPoseParameterRange(self.PoseParamsMap[self.LampPoseParams[2]])
+		if dir == MOVE_LEFT then
+			local newVal = self.HorizontalParam + manip
+			if newVal <= max then
+				self.HorizontalParam = newVal
+			else
+				self.HorizontalParam = max
+			end
+		else
+			local newVal = self.HorizontalParam - manip
+			if newVal >= min then
+				self.HorizontalParam = newVal
+			else
+				self.HorizontalParam = min
+			end
+		end
+	end
 end
 
 hook.Add("EntityNetworkedVarChanged", "SpotlightPrototype", function(ent, name, oldval, newval)
@@ -90,24 +133,17 @@ hook.Add("EntityNetworkedVarChanged", "SpotlightPrototype", function(ent, name, 
 	end
 end)
 
-local move_amt = 2
-
 hook.Add("Tick", "SpotlightPrototype", function()
 	if not IsValid(SPOTLIGHT_TARGET) then return end
 	local ent = SPOTLIGHT_TARGET
-	local val_yaw = ent.YawParam
-	local val_pitch = ent.PitchParam
+	if not ent.PoseReady then return end
 	if input.IsKeyDown(KEY_LEFT) then
-		print("LEFT")
-		ent.YawParam = val_yaw + move_amt
+		ent:Adjust(MOVE_LEFT)
 	elseif input.IsKeyDown(KEY_RIGHT) then
-		print("RIGHT")
-		ent.YawParam = val_yaw - move_amt
+		ent:Adjust(MOVE_RIGHT)
 	elseif input.IsKeyDown(KEY_UP) then
-		print("UP")
-		ent.PitchParam = val_pitch - move_amt
+		ent:Adjust(MOVE_UP)
 	elseif input.IsKeyDown(KEY_DOWN) then
-		print("DOWN")
-		ent.PitchParam = val_pitch + move_amt
+		ent:Adjust(MOVE_DOWN)
 	end
 end)
--- lua/entities/ent_spotlight_prototype/shared.lua
+++ lua/entities/ent_spotlight_prototype/shared.lua
@@ -1,6 +1,8 @@
 print("[NP Spot]: shared.lua")
 AddCSLuaFile()
 
+ENT.Model = "models/np_spotlight/test_spotlight.mdl"
+
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
 ENT.PrintName = "Spotlight Prototype"
@@ -8,15 +10,22 @@ ENT.Author = "Schmal, et al."
 ENT.Category = "Not Photon"
 ENT.Spawnable = true
 
+-- How fast/slow turning should be
+-- (percent per tick)
+ENT.ManipulationRate = 0.015
+
+-- Name of bone that light emits from
 ENT.LightBone = "light"
 
 ENT.LampOffsetPos = Vector(0, 0, 0)
 ENT.LampOffsetAng = Angle(0, -90, 0)
 
+-- Material index and name to use when light is active
 ENT.LampSubMaterials = {
 	[2] = "np_spotlight/illum"
 }
 
+-- Configures turn axes with pose parameters
 ENT.LampPoseParams = {
 	-- Up/Down Arrows
 	[1] = "point_pitch",
@@ -24,8 +33,26 @@ ENT.LampPoseParams = {
 	[2] = "point_yaw"
 }
 
-ENT.Model = "models/np_spotlight/test_spotlight.mdl"
+-- fucking dumb
+function ENT:SetupPoseParamsMap()
+	self.PoseParamsMap = {}
+	for i=0, self:GetNumPoseParameters() - 1 do
+		local name = self:GetPoseParameterName(i)
+		self.PoseParamsMap[name] = 0
+	end
+	self.PoseManipulationRates = {}
+	for _, param in pairs(self.LampPoseParams) do
+		local min, max = self:GetPoseParameterRange(self.PoseParamsMap[param])
+		local range = max - min
+		self.PoseManipulationRates[param] = range * self.ManipulationRate
+	end
+	self.PoseReady = true
+end
 
 function ENT:GetLightEnabled()
 	return self:GetNW2Bool("lamp.enabled")
+end
+
+function ENT:Initialize()
+	print("shared init")
 end
